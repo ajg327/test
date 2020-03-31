a8522e58741e825b699145a298191e684eb53401
Andrew Granja <andrewgranja@macbook-pro.attlocal.net>
2020-03-31 12:24:47 -0400

    Finished Debug 4

---
+++ class/Class9-2/js/jQuery2.js
@@ -0,0 +1,60 @@
+$(document).ready( function(){
+
+
+$("#_hide").click(function(){
+  $(".gridContent div:first-child p").hide();
+});
+
+$("#_show").click(function(){
+  $(".gridContent div:first-child p").show();
+});
+
+$("#_toggle").click( function(){
+  $(".gridContent div:nth-child(2) p").toggle();
+});
+
+$("#_html").click(function() {
+  $(".gridContent div:nth-child(3) p").html("Hello <strong>World</strong>!");
+});
+
+$("#_css").click(function(){
+  $(".box").css("background-color", "lightblue");
+});
+
+
+$("footer").mouseover(function(){
+    $("footer p").html("Yeah! You're over me");
+});
+
+$("footer").mouseleave(function() {
+  $("footer p").html("Comeback...=(");
+});
+
+
+
+// --------------------- Debug #4 ----------------------------
+
+// Here goes your jQuery :
+
+$("#_debughide").click(function(){
+  $(".smallGridBug div:first-child p").hide();
+});
+
+$("#_debugshow").click(function(){
+  $(".smallGridBug div:first-child p").show();
+});
+
+$("#_debugtoggle").click( function(){
+  $(".smallGridBug div:nth-child(2) p").toggle();
+});
+
+$("#_debughtml").click(function() {
+  $(".smallGridBug div:nth-child(3) p").html("Hello! I changed the HTML to a different text!");
+});
+
+$("#_debugcss").click(function(){
+  $(".smallGridBug").css("background-color", "lightblue");
+});
+
+
+});
