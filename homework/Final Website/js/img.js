e02f11c10f043d8009ef3499c6446f2451d3f568
Andrew Granja <andrewgranja@macbook-pro.attlocal.net>
2020-05-04 19:41:45 -0400

    Finished JS. finished Final!

---
+++ homework/Final Website/js/img.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#_mangag").click(function(){
+      $(".openingbox div:nth-child(4) img").attr("src", "../IMGS/mangaG.jpg");
+    });
+    $("#_animeg").click(function(){
+      $(".openingbox div:nth-child(4) img").attr("src", "../IMGS/Goku(big).jpg");
+    });
+    $("#_mangav").click(function(){
+      $(".Vegetabox div:nth-child(4) img").attr("src", "../IMGS/mangaV.jpg");
+    });
+    $("#_animev").click(function(){
+      $(".Vegetabox div:nth-child(4) img").attr("src", "../IMGS/Vegeta.jpg");
+    });
+    $("#_mangag2").click(function(){
+      $(".Gohanbox div:nth-child(4) img").attr("src", "../IMGS/GohanM.jpeg");
+    });
+    $("#_animeg2").click(function(){
+      $(".Gohanbox div:nth-child(4) img").attr("src", "../IMGS/gohan.png");
+    });
+    $("#_mangaf").click(function(){
+      $(".Friezabox div:nth-child(4) img").attr("src", "../IMGS/mangaF.png");
+    });
+    $("#_animef").click(function(){
+      $(".Friezabox div:nth-child(4) img").attr("src", "../IMGS/Frieza.png");
+    });
+});
